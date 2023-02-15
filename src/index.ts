cddf17da44ddb6738a47819175ed6228431610b7
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-15 20:17:26 +0530

    initialized nodeTS project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log("Hi from index.ts");
+console.log("use the command 'npm run dev' to run the typescript files");
+console.log(
+  "use the command 'npm run build' to compile the typescript files to javascript files"
+);
+console.log("use the command 'npm run prod' to run the javascript files");
+console.log(
+  "make sure to use .ts in the import statements for typescript files"
+);
